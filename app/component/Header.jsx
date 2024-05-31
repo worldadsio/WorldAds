9f7bf1a0260581f2f2c30228e9ea2445644d14c9
Tahir Ramzan <tahirramzan1@yahoo.com>
2024-05-31 12:36:01 +0500

    Logo and Linkedin

--- app/component/Header.jsx
+++ app/component/Header.jsx
@@ -1,92 +1,157 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import NavContent from './NavContent';
+"use client";
+import React, { useState, useEffect } from "react";
 import { RxCross2 } from "react-icons/rx";
+import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
 
 const Header = () => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [isHeaderFixed, setIsHeaderFixed] = useState(false);
-    const [open, setOpen] = useState(false);
-    const [drop, setDrop] = useState(false)
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
 
-    const handleDrawerToggle = () => {
-        setIsDrawerOpen(!isDrawerOpen);
-        console.log('Drawer open state:', isDrawerOpen);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+    console.log("Drawer open state:", isDrawerOpen);
+  };
+
+  const dropDownOpen = () => {
+    setOpen(!open);
+    setDrop(!drop);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      if (scrollPosition > 10) {
+        setIsHeaderFixed(true);
+      } else {
+        setIsHeaderFixed(false);
+      }
     };
 
-    const dropDownOpen = () => {
-        setOpen(!open);
-        setDrop(!drop)
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      const topOffset = section.offsetTop;
+      const windowHeight = window.innerHeight;
+      const scrollTo =
+        topOffset - (windowHeight / 2 - section.offsetHeight / 2);
+      window.scrollTo({ top: scrollTo, behavior: "smooth" });
     }
+  };
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollPosition = window.scrollY;
+  const handleNavigation = (sectionId) => {
+    if (pathname !== "/") {
+      router.push(`/#${sectionId}`);
+    } else {
+      scrollToSection(sectionId);
+    }
+  };
 
-            if (scrollPosition > 10) {
-                setIsHeaderFixed(true);
-            } else {
-                setIsHeaderFixed(false);
-            }
-        };
+  return (
+    <div className={`fixed w-full z-40 top-0 bg-[#BF2506] py-6 lg:p-7`}>
+      <div className="relative w-[85%] mx-auto flex justify-between items-center bg-transparent">
+        <Link href="/">
+          <img
+            src="./assets/images/logo.png"
+            alt="Logo"
+            className="lg:w-52 lg:h-12 w-36 h-8"
+            style={{ cursor: "pointer" }}
+          />
+        </Link>
+        <button
+          className="bg-[#FFFFFF1A] border-2 border-white lg:px-6 lg:py-3 px-3 py-1 hover:border rounded-[100px]"
+          type="button"
+          onClick={dropDownOpen}
+        >
+          <img src="./assets/images/bars.svg" alt="Menu" className="w-6" />
+        </button>
 
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    const scrollToSection = (sectionId) => {
-        const section = document.getElementById(sectionId);
-        if (section) {
-            const topOffset = section.offsetTop;
-            const windowHeight = window.innerHeight;
-            const scrollTo = topOffset - (windowHeight / 2 - section.offsetHeight / 2);
-            window.scrollTo({ top: scrollTo, behavior: 'smooth' });
-        }
-    };
-
-    return (
-        <div className={`fixed w-full z-40 top-0  bg-[#BF2506] py-6 lg:p-7`}>
-            <div className="relative w-[85%] mx-auto flex justify-between items-center bg-transparent">
-                <img src="./assets/images/logo.png" alt="Logo" className="lg:w-52 lg:h-12 w-36 h-8" />
-                <button
-                    className="bg-[#FFFFFF1A]   border-2 border-white lg:px-6 lg:py-3 px-3 py-1 hover:border rounded-[100px]"
-                    type="button"
-                    onClick={dropDownOpen}
+        {drop && (
+          <div
+            className="w-[100%] lg:hidden block gradient-color absolute top-0 right-0 left-0 border border-white rounded-2xl h-60"
+            style={{ backdropFilter: "blur(50px)" }}
+          >
+            <ul className="px-5 space-y-3 text-center text-lg text-white">
+              <li>
+                <RxCross2 className="text-2xl font-bold float-right" />
+              </li>
+              <li className="pt-10">
+                <Link href="/">About</Link>
+              </li>
+              <li>
+                <a
+                  href="#nodeOperator"
+                  onClick={() => handleNavigation("nodeOperator")}
                 >
-                    {drop && (
-                        <div className='w-[100%] lg:hidden block gradient-color absolute top-0 right-0 left-0 border border-white rounded-2xl h-60'
-                            style={{ backdropFilter: 'blur(50px)' }}>
-                            <ul className="px-5 space-y-3 text-center text-lg text-white">
-                                <li><RxCross2 className="text-2xl font-bold float-right" /></li>
-                                <li className='pt-10'><a href="#">About</a></li>
-                                <li><a href="#nodeOperator" onClick={() => scrollToSection('nodeOperator')}>Node Operators</a></li>
-                                <li><a href="#ride">Ride & Earn</a></li>
-                                <li><a href="#advertise">Advertise With WorldAds</a></li>
-                            </ul>
-                        </div>
-                    )}
+                  Node Operators
+                </a>
+              </li>
+              <li>
+                <a href="#ride" onClick={() => handleNavigation("ride")}>
+                  Ride & Earn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#advertise"
+                  onClick={() => handleNavigation("advertise")}
+                >
+                  Advertise With WorldAds
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
 
-
-                    {open && (
-                        <div className="absolute lg:block hidden top-0 right-0 border-2 bg-red-600 border-white rounded-full p-2">
-                            <ul className="px-5 flex items-center space-x-5 text-lg text-white justify-between">
-                                <li><a href="#">About</a></li>
-                                <li><a href="#nodeOperator" onClick={() => scrollToSection('nodeOperator')}>Node Operators</a></li>
-                                <li><a href="#ride">Ride & Earn</a></li>
-                                <li><a href="#advertise">Advertise With WorldAds</a></li>
-                                <li><RxCross2 className="text-2xl font-bold" /></li>
-                            </ul>
-                        </div>
-                    )}
-
-                    <img src="./assets/images/bars.svg" alt="Menu" className="w-6" />
-                </button>
-            </div>
-        </div>
-    );
+        {open && (
+          <div className="absolute lg:block hidden top-0 right-0 border-2 bg-red-600 border-white rounded-full p-2">
+            <ul className="px-5 flex items-center space-x-5 text-lg text-white justify-between">
+              <li>
+                <Link href="/">About</Link>
+              </li>
+              <li>
+                <a
+                  href="#nodeOperator"
+                  onClick={() => handleNavigation("nodeOperator")}
+                >
+                  Node Operators
+                </a>
+              </li>
+              <li>
+                <a href="#ride" onClick={() => handleNavigation("ride")}>
+                  Ride & Earn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#advertise"
+                  onClick={() => handleNavigation("advertise")}
+                >
+                  Advertise With WorldAds
+                </a>
+              </li>
+              <li>
+                <RxCross2 className="text-2xl font-bold" />
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- app/component/footer.jsx
+++ app/component/footer.jsx
@@ -1,57 +1,103 @@
-"use client"
+"use client";
 import Image from "next/image";
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { MdOutlineArticle } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
 import { MdOutlinePolicy } from "react-icons/md";
 import { MdOutlineContactPage } from "react-icons/md";
+import { MdAddBusiness } from "react-icons/md";
 
 export default function Footer() {
-    const router = useRouter();
+  const router = useRouter();
 
-    function goToPage(path) {
-        router.push(`${path}`);
-    }
+  function goToPage(path) {
+    router.push(`${path}`);
+  }
 
-    return (
-        <div className="bg-[#181818] ] w-full mt-20 lg:py-12 pt-12 lg:p-4">
-            <div className="lg:w-[80%] w-[90%] lg:flex items-center justify-between mx-auto py-12" >
-                <div>
-                    <h1 className="lg:text-5xl text-3xl  lg:w-[500px] w-[250px] lg:mx-0 mx-auto font-medium lg:text-start text-center text-white">Innovating Digital Advertising</h1>
-                </div>
-                <div className="lg:flex text-white lg:mt-0 mt-10 items-center gap-10">
-                    <div>
-                        <ul className="text-lg lg:mt-0 mt-3  lg:text-start text-center space-y-3">
-                        <Link href="https://worldadsio.medium.com/">
-                                <li className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2" onClick={() => goToPage("/blogs")}><MdOutlineArticle />Blog</li>
-
-                            </Link>
-                            <Link href="/privacy">
-                                <li className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2" onClick={() => goToPage("/privacy")}><MdOutlinePolicy />Privacy Policy</li>
-
-                            </Link>
-                            <Link href="/terms">
-                            <li className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2" onClick={() => goToPage("/terms")}><MdOutlineContactPage />
-                                Terms Of Service</li>
-                            </Link>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div className="text-white relative mx-auto mt-5 lg:w-[80%]">
-                <div className="flex items-center gap-1 w-40 lg:w-[80%] lg:mx-0 mx-auto">
-                    <Image src="/Group.png" alt="logo" width={37.49} height={37.49} className="lo " />
-                    <h1 className="text-2xl font-meidum">WorldsAds</h1>
-                </div>
-                <div className="flex items-center lg:justify-start w-[90%] lg:mx-0 mx-auto lg:w-[80%] justify-center mt-5 gap-10">
-                    <p className="lg:text-sm text-[12px] lg:text-start text-center  font-light ">©2024 WorldAds Tech DAO LLC. All Right Reserved </p>
-                    <hr className="border lg:block hidden border-[#707070] w-96" />
-                </div>
-                <Image src="/FooterImage.png" alt="vector" width={231.11} height={231.11} className="absolute lg:-top-16 lg:block hidden z-20 lg:-right-36 " />
-                <Image src="/FooterImage (1).png" alt="vector" width={551.11} height={451.11} className=" lg:hidden block mt-10 " />
-
-            </div>
+  return (
+    <div className="bg-[#181818] ] w-full mt-20 lg:py-12 pt-12 lg:p-4">
+      <div className="lg:w-[80%] w-[90%] lg:flex items-center justify-between mx-auto py-12">
+        <div>
+          <h1 className="lg:text-5xl text-3xl  lg:w-[500px] w-[250px] lg:mx-0 mx-auto font-medium lg:text-start text-center text-white">
+            Innovating Digital Advertising
+          </h1>
         </div>
-    )
+        <div className="lg:flex text-white lg:mt-0 mt-10 items-center gap-10">
+          <div>
+            <ul className="text-lg lg:mt-0 mt-3  lg:text-start text-center space-y-3">
+              <Link href="https://worldadsio.medium.com/">
+                <li
+                  className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2"
+                  onClick={() => goToPage("/blogs")}
+                >
+                  <MdOutlineArticle />
+                  Blog
+                </li>
+              </Link>
+              <Link href="https://linkedin.com/">
+                <li
+                  className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2"
+                  onClick={() => goToPage("/blogs")}
+                >
+                  <MdAddBusiness />
+                  Linkedin
+                </li>
+              </Link>
+              <Link href="/privacy">
+                <li
+                  className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2"
+                  onClick={() => goToPage("/privacy")}
+                >
+                  <MdOutlinePolicy />
+                  Privacy Policy
+                </li>
+              </Link>
+              <Link href="/terms">
+                <li
+                  className="hover:text-gray-300 justify-center lg:justify-start cursor-pointer flex items-center gap-2"
+                  onClick={() => goToPage("/terms")}
+                >
+                  <MdOutlineContactPage />
+                  Terms Of Service
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="text-white relative mx-auto mt-5 lg:w-[80%]">
+        <div className="flex items-center gap-1 w-40 lg:w-[80%] lg:mx-0 mx-auto">
+          <Image
+            src="/Group.png"
+            alt="logo"
+            width={37.49}
+            height={37.49}
+            className="lo "
+          />
+          <h1 className="text-2xl font-meidum">WorldsAds</h1>
+        </div>
+        <div className="flex items-center lg:justify-start w-[90%] lg:mx-0 mx-auto lg:w-[80%] justify-center mt-5 gap-10">
+          <p className="lg:text-sm text-[12px] lg:text-start text-center  font-light ">
+            ©2024 WorldAds Tech DAO LLC. All Right Reserved{" "}
+          </p>
+          <hr className="border lg:block hidden border-[#707070] w-96" />
+        </div>
+        <Image
+          src="/FooterImage.png"
+          alt="vector"
+          width={231.11}
+          height={231.11}
+          className="absolute lg:-top-16 lg:block hidden z-20 lg:-right-36 "
+        />
+        <Image
+          src="/FooterImage (1).png"
+          alt="vector"
+          width={551.11}
+          height={451.11}
+          className=" lg:hidden block mt-10 "
+        />
+      </div>
+    </div>
+  );
 }
